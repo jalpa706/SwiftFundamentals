8a8abf73dd48e6cde1626fdd37481b6148115014
iFlame <iflame@Administrators-iMac.local>
2017-06-13 08:54:18 +0530

    Initial Commit

---
+++ FundamentalSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FundamentalSwift
+//
+//  Created by iFlame on 6/13/17.
+//  Copyright © 2017 iFlame. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
